0e2043be620a8cd83acf492342eca125a1471c92
ag597482 <ag597482@gmai.com>
2020-06-21 23:01:28 +0530

    authentation added

--- app/src/main/java/com/Indra/nwtk/frag5.java
+++ app/src/main/java/com/Indra/nwtk/frag5.java
@@ -7,58 +7,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.Indra.nwtk.MainActivity.mUsername;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link frag5#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class frag5 extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public frag5() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment frag5.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static frag5 newInstance(String param1, String param2) {
-        frag5 fragment = new frag5();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag5, container, false);
+        View view=inflater.inflate(R.layout.fragment_frag5, container, false);
+
+        TextView name = view.findViewById(R.id.username);
+        TextView image = view.findViewById(R.id.user_image);
+
+        name.setText(mUsername);
+        image.setText(String.valueOf(mUsername.charAt(0)).toUpperCase());
+
+
+        return view;
     }
 }
